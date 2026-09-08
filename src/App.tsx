1422c0a7b9665c94092e921b4d38132bc82e52f8
Andrej Basharin <basharin_andrej@mail.ru>
2026-09-09 00:14:43 +0300

    add single compoennts

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,16 @@
-import useTodoStore from '../store/todoStore.ts';
+import useTodoStore from '../store/todoStore';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import TaskListPage from './TaskListPage';
+import TaskFormPage from './TaskFormPage';
 
 function App() {
   const { todos, addTodo, toggleTodo, removeTodo } = useTodoStore();
 
-  const handleAddTodo = () => {
-    const text = prompt('Enter todo text:');
-    if (text) {
-      addTodo(text);
-    }
-  };
-
-  
   return (
     <div className="container mt-4">
       <h1>Todo App</h1>
-      <button onClick={handleAddTodo} className="btn btn-primary mb-3">
-        Add Todo
-      </button>
-      <ul className="list-group">
-        {todos.map((todo) => (
-          <li key={todo.id} className="list-group-item d-flex justify-content-between align-items-center">
-            <span
-              style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}
-              onClick={() => toggleTodo(todo.id)}
-            >
-              {todo.text}
-            </span>
-            <button onClick={() => removeTodo(todo.id)} className="btn btn-danger btn-sm">
-              Remove
-            </button>
-          </li>
-        ))}
-      </ul>
+      <TaskFormPage addTodo={addTodo} />
+      <TaskListPage todos={todos} toggleTodo={toggleTodo} removeTodo={removeTodo} />
     </div>
   );
 }
