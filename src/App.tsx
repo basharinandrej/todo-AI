0504dc78ff1372a74bcde86e60fa161268267a0f
Andrej Basharin <basharin_andrej@mail.ru>
2026-09-10 01:02:51 +0300

    feat: call loadTodos on app mount to hydrate state from IndexedDB

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import { useEffect } from 'react';
 import useTodoStore from '../store/todoStore';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import TaskListPage from './TaskListPage';
 import TaskFormPage from './TaskFormPage';
 
 function App() {
-  const { todos, addTodo, toggleTodo, removeTodo } = useTodoStore();
+  const { todos, loadTodos, addTodo, toggleTodo, removeTodo } = useTodoStore();
+
+  useEffect(() => {
+    loadTodos();
+  }, [loadTodos]);
 
   return (
     <div className="container mt-4">
